51dc952c26d249f45af29a30b83ba6e576de6bb2
Marsel Shakirov <imanager666@gmail.com>
2023-04-24 08:49:07 +0300

    add_toggle_categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 
 function Categories() {
+  const [activeType, setActiveType] = React.useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {/* <li className="active">Все</li> */}
+        {categories.map((type, i) => {
+          return (
+            <li
+              onClick={() => setActiveType(i)}
+              key={`${type}_${i}`}
+              className={activeType === i ? "active" : ""}
+            >
+              {type}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
